534a90bd76c45f6ccebd62a5ed6e124440343ddb
devgsanches <guilhermesanches.pessoal@gmail.com>
2025-07-25 16:37:45 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Barbearia</h1>
+      <Button>Agendar</Button>
     </div>
   );
 }
